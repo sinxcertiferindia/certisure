78555cd96cb1697f3fecbd706bdefff1e25a72da
manasparihar <kotharimanas71@gmail.com>
2026-01-12 17:34:43 +0530

    email support hataya hai

--- src/pages/Templates.tsx
+++ src/pages/Templates.tsx
@@ -87,7 +87,7 @@ const Templates = () => {
             </motion.div>
 
             {/* Email Templates Card */}
-            <motion.div
+            { /*<motion.div
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.3, delay: 0.1 }}
@@ -126,7 +126,7 @@ const Templates = () => {
                   </div>
                 </CardContent>
               </Card>
-            </motion.div>
+            </motion.div>*/}
           </div>
         </div>
       </main>
